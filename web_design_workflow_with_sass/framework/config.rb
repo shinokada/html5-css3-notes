a291819b01d215588c8578ed18eb99184a5a04d8
Shinichi Okada <sokada@Shinichis-MacBook-Pro.local>
2015-02-05 21:06:42 +0900

    Added web design workflow with sass

---
+++ web_design_workflow_with_sass/framework/config.rb
@@ -0,0 +1,21 @@
+# Require any additional compass plugins here.
+
+http_path = "/"
+css_dir = "css"
+sass_dir = "sass"
+images_dir = "images"
+javascripts_dir = "javascripts"
+
+output_style = :expanded
+
+relative_assets = true
+
+line_comments = false
+
+
+# If you prefer the indented syntax, you might want to regenerate this
+# project again passing --syntax sass, or you can uncomment this:
+# preferred_syntax = :sass
+# and then run:
+# sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
+preferred_syntax = :scss
